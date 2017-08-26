1e47dcb7388af704e7ec392e9eb29cb4b5318963
Christopher Lee <lee@foldmountain.com>
2017-08-26 06:58:23 -0400

    linter cleanup

--- src/boolparser/__init__.py
+++ src/boolparser/__init__.py
@@ -1,9 +1,10 @@
-
+"""Boolean Parser library."""
 from boolparser.core import *
+
 
 def test_parser():
     import sys
-    test_string="""a>1
+    test_string = """a>1
 a<1
 a>=1
 a<=1
@@ -13,25 +14,26 @@ a==1 & ! b
 (a==b)
 (a==b) & (c>1)
 ((a==b) & (c>1)) | d<=2
-(a==b) & (c>1) | d<=2 
+(a==b) & (c>1) | d<=2
 (((a==b) & (c>1)) & (d<=2) & e>=1.5)
 ((a==b) & (c>1)) & (d<=2) & e>=1.5
 ((a==b) & (c>1)) | (d<=2) | ((e>=1.5) & (g==0) & ! h)((a==b) & (c>1)) | (d<=2) | ((e>=1.5) & (g==0) & ! h)
 """
-    
-    b = dict(a=4,b=5,c=6,d=7,e=8,f=9,g=10,h=False)
+
+    b = dict(a=4, b=5, c=6, d=7, e=8, f=9, g=10, h=False)
+
     class ev_dict(EvaluateVariable):
         def eval(self):
-            #self.value is available
-            return b.get(self.value,False)
-        
+            # self.value is available
+            return b.get(self.value, False)
+
     bp = BoolParser(ev_dict)
-    if len(sys.argv)>=1:
-        if sys.argv[1]=="--help":
+    if len(sys.argv) >= 1:
+        if sys.argv[1] == "--help":
             print("This is just a dumb test program")
         sys.exit(0)
-        
-    if len(sys.argv)==1:
+
+    if len(sys.argv) == 1:
         for line in test_string.split("\n"):
             if line:
                 print(line, bp.parse(line))
--- src/boolparser/core.py
+++ src/boolparser/core.py
@@ -1,4 +1,6 @@
-"""Parsing module to parse a simple grammar of boolean operations, such as 
+"""Parsing module to parse a simple grammar of boolean operations.
+
+For example:
 a>1
 a<1
 a>=1
@@ -9,34 +11,44 @@ a==1 & ! b
 (a==b)
 (a==b) & (c>1)
 ((a==b) & (c>1)) | d<=2
-(a==b) & (c>1) | d<=2 
+(a==b) & (c>1) | d<=2
 (((a==b) & (c>1)) & (d<=2) & e>=1.5)
 ((a==b) & (c>1)) & (d<=2) & e>=1.5
 ((a==b) & (c>1)) | (d<=2) | ((e>=1.5) & (g==0) & ! h)
 """
 
-from pyparsing import *
+from pyparsing import Regex, Word, oneOf, operatorPrecedence, Forward,\
+                      alphanums, opAssoc
+
 
 class EvaluateNumber(object):
-    """Class to evaluate a number by converting it into a float"""
+    """Class to evaluate a number by converting it into a float."""
+
     def __init__(self, tokens):
         self.value = tokens[0]
+
     def eval(self):
         return float(self.value)
+
     def __str__(self):
         return str(float(self.value))
 
+
 class EvaluateVariable(object):
-    """Class to evaluate a variable by calling the 'get' function"""
+    """Class to evaluate a variable by calling the 'get' function."""
+
     def __init__(self, tokens):
         self.value = tokens[0]
+
     def eval(self):
         return get(self.value)
+
     def __str__(self):
         return str(self.value)
 
+
 def opPair(tokenlist):
-    """Given a list of tokens, returns pairs of tokens to the caller as a generator"""
+    """Return pairs of tokens to the caller as a generator."""
     it = iter(tokenlist)
     while 1:
         try:
@@ -44,74 +56,84 @@ def opPair(tokenlist):
         except StopIteration:
             break
 
+
 class EvaluateComparison(object):
-    """Class to define the comparison operators. When eval is called the input is list is
-    passed to the opPair generator and each pair is evaluated"""
+    """Class to define the comparison operators.
+
+    When eval is called the input is list is
+    passed to the opPair generator and each pair is evaluated."""
+
     operators = {
-        "<" :  ["lessthan",lambda a,b : a < b],
-        ">" :  ["greaterthan",lambda a,b : a > b],
-        ">=" : ["greaterequals",lambda a,b : a >= b],
-        "<=" : ["lessequals",lambda a,b : a <= b],
-        "==" : ["equals",lambda a,b : a == b],
-        "!=" : ["notequal",lambda a,b : a != b],
+        "<":  ["lessthan", lambda a, b: a < b],
+        ">":  ["greaterthan", lambda a, b: a > b],
+        ">=": ["greaterequals", lambda a, b: a >= b],
+        "<=": ["lessequals", lambda a, b: a <= b],
+        "==": ["equals", lambda a, b: a == b],
+        "!=": ["notequal", lambda a, b: a != b],
         }
+
     def __init__(self, tokens):
         self.value = tokens[0]
-        #print map(str,self.value)
-    
+
     def eval(self):
         val1 = self.value[0].eval()
-        for op,val in opPair(self.value[1:]):
+        for op, val in opPair(self.value[1:]):
             fn = EvaluateComparison.operators[op][1]
             val2 = val.eval()
-            val1 =  fn(val1,val2)
+            val1 = fn(val1, val2)
         return val1
-    
+
     def __str__(self):
-        return "({0} {1} {2})".format(  str(self.value[0]), 
-                                    EvaluateComparison.operators[self.value[1]][0],
-                                    str(self.value[2]))
+        val = EvaluateComparison.operators[self.value[1]][0]
+        return "({0} {1} {2})".format(str(self.value[0]),
+                                      val,
+                                      str(self.value[2]))
+
 
 class EvaluateOrAnd(object):
-    """Special class to evaluate an AND or OR"""
+    """Special class to evaluate an AND or OR."""
+
     operators = {
-        "&" : ["and",lambda a,b : a & b],
-        "|" : ["or",lambda a,b : a | b],
+        "&": ["and", lambda a, b: a & b],
+        "|": ["or", lambda a, b: a | b],
         }
+
     def __init__(self, tokens):
         self.value = tokens[0]
-    
+
     def eval(self):
         val1 = self.value[0].eval()
-        for op,val in opPair(self.value[1:]):
+        for op, val in opPair(self.value[1:]):
             fn = EvaluateOrAnd.operators[op][1]
             val2 = val.eval()
-            val1 = fn(val1,val2)
+            val1 = fn(val1, val2)
         return val1
-    
+
     def __str__(self):
         string = "("
         val1 = str(self.value[0])
-        for op,val in opPair(self.value[1:]):
+        for op, val in opPair(self.value[1:]):
             fn = EvaluateOrAnd.operators[op][0]
             val2 = val
-            string = string + "{0}, {1}, {2}".format(val1, fn,val2)
+            string = string + "{0}, {1}, {2}".format(val1, fn, val2)
         string = string + ")"
         return string
 
+
 class EvaluateNot(object):
-    """Class to evaluate not, which only takes one argument"""
+    """Class to evaluate not, which only takes one argument."""
+
     operators = {
-        "!" : ["(not)",lambda a : not a]
+        "!": ["(not)", lambda a: not a]
         }
-    
+
     def __init__(self, tokens):
         self.value = tokens[0]
 
     def eval(self):
-        for op,val in opPair(self.value[0:]):
+        for op, val in opPair(self.value[0:]):
             fn = EvaluateNot.operators[op][1]
-            val2=val.eval()
+            val2 = val.eval()
             val = fn(val2)
         return val
 
@@ -119,53 +141,69 @@ class EvaluateNot(object):
         return " ".join([str(v) for v in self.value])
 
 
-#parser definition
 class BoolParser(object):
+    """Parser definition."""
+
     def __init__(self, EvaluateVariableChild=None, EvaluateNumberChild=None):
         EvaluateVariableChild = EvaluateVariableChild or EvaluateVariable
         EvaluateNumberChild = EvaluateNumberChild or EvaluateNumber
-        #what is a float number
+        # what is a float number
         floatNumber = Regex(r'[-]?\d+(\.\d*)?([eE][-+]?\d+)?')
-        #a variable is a combination of letters, numbers, and underscor
+        # a variable is a combination of letters, numbers, and underscor
         variable = Word(alphanums + "_")
-        #a sign is plus or minus
+        # a sign is plus or minus
         signOp = oneOf('+ -')
-        #an operand is a variable or a floating point number
+        # an operand is a variable or a floating point number
         operand = floatNumber ^ variable
-        #when a floatNumber is found, parse it with evaluate number
+        # when a floatNumber is found, parse it with evaluate number
         floatNumber.setParseAction(EvaluateNumberChild)
-        #when a variable is found, parse it with the EvaluateVariableChild or EvaluateVariable
+        # when a variable is found, parse it with the EvaluateVariableChild
+        # or EvaluateVariable
         variable.setParseAction(EvaluateVariableChild)
-        #comparisons include lt,le,gt,ge,eq,ne
+        # comparisons include lt,le,gt,ge,eq,ne
         comparisonOp = oneOf("< <= > >= == !=")
-        #negation of the boolean is !
+        # negation of the boolean is !
         notOp = oneOf("!")
-        #an expression is a either a comparison or a NOT operation (where NOT a is essentially (a == False))
+        # an expression is a either a comparison or
+        # a NOT operation (where NOT a is essentially (a == False))
         comparisonExpression = operatorPrecedence(operand,
-            [
-                (comparisonOp, 2, opAssoc.LEFT, EvaluateComparison),
-                (notOp, 1, opAssoc.RIGHT, EvaluateNot),
-            ])
-        
-        #boolean logic of AND or OR
+                                                  [
+                                                   (comparisonOp,
+                                                    2,
+                                                    opAssoc.LEFT,
+                                                    EvaluateComparison
+                                                    ),
+                                                   (notOp,
+                                                    1,
+                                                    opAssoc.RIGHT,
+                                                    EvaluateNot
+                                                    ),
+                                                  ])
+
+        # boolean logic of AND or OR
         boolOp = oneOf("& |")
-        
-        #a bool expression contains a nested bool expression or a comparison, joined with a boolean operation
+
+        # a bool expression contains a nested bool expression or a comparison,
+        # joined with a boolean operation
         boolExpression = Forward()
         boolPossible = boolExpression | comparisonExpression
         self.boolExpression = operatorPrecedence(boolPossible,
-            [
-            (boolOp, 2, opAssoc.RIGHT, EvaluateOrAnd),
-            ])
+                                                 [
+                                                  (boolOp,
+                                                   2,
+                                                   opAssoc.RIGHT,
+                                                   EvaluateOrAnd
+                                                   ),
+                                                 ])
         return
 
-    def parseString(self,line):
-        if len(line): 
+    def parseString(self, line):
+        """Parser a string and print the expression."""
+        if len(line):
             print(self.boolExpression.parseString(line)[0])
 
-    def parse(self,line):
+    def parse(self, line):
+        """Parse a string and return the result."""
         if len(line):
-            data= self.boolExpression.parseString(line)[0]
+            data = self.boolExpression.parseString(line)[0]
             return data.eval()
-
-        
