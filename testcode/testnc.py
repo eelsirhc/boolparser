a6b8cea24d761c4ad010defa243f5d38d30a27a6
Christopher Lee <lee@foldmountain.com>
2017-08-26 06:56:08 -0400

    update testcode

--- testcode/testnc.py
+++ testcode/testnc.py
@@ -1,43 +1,64 @@
-import netCDF4
-from boolparser.core import BoolParser, EvaluateVariable
 
-class ncar(object):
-    def __init__(self,var):
-        self.var = var
-        return
-    def __ge__(self, num):
-        return self.var>=num
-    def __le__(self, num):
-        return self.var<=num
-    def __gt__(self, num):
-        return self.var>num
-    def __lt__(self, num):
-        return self.var<num
+
+
+def test_trivial():
+    from boolparser import BoolParser, EvaluateVariable
     
-def define_class(nc):
-    class ev_nc(EvaluateVariable):
-        def eval(self):
-            if self.value in nc.variables:
-                return ncar(nc.variables[self.value][:])
-            elif self.value in nc.dimensions:
-                return ncar(nc.dimensions[self.value][:])
-            else:
-                raise NameError("Not a variable: {0}".format(self.value))
-                return self.value
-    return ev_nc
-    
-nc = netCDF4.Dataset("mrom2001.nc")
+    d=dict(a=1,b=2,c=3,d=4)
+    def define_class(dic):
+        class d_ev(EvaluateVariable):
+            def eval(self):
+                return dic[self.value]
+        return d_ev
+    bp = BoolParser(define_class(d))
+    assert bp.parse("a==1")
+    assert not bp.parse("a>1")
+    assert not bp.parse("!b==2")
+    assert not bp.parse("!(b==2)")
+    assert bp.parse("!b==0")
 
-nceval = define_class(nc)
+def test_nc():
+    import netCDF4
+    import os
+    from boolparser.core import BoolParser, EvaluateVariable
 
-bp   = BoolParser(nceval)
+    class ncar(object):
+        def __init__(self,var):
+            self.var = var
+            return
+        def __ge__(self, num):
+            return self.var>=num
+        def __le__(self, num):
+            return self.var<=num
+        def __gt__(self, num):
+            return self.var>num
+        def __lt__(self, num):
+            return self.var<num
+
+    def define_class(nc):
+        class ev_nc(EvaluateVariable):
+            def eval(self):
+                if self.value in nc.variables:
+                    return ncar(nc.variables[self.value][:])
+                elif self.value in nc.dimensions:
+                    return ncar(nc.dimensions[self.value][:])
+                else:
+                    raise NameError("Not a variable: {0}".format(self.value))
+                    return self.value
+        return ev_nc
+
+    _ROOT = os.path.abspath(os.path.dirname(__file__))
+    filename = os.path.join(_ROOT, 'data', "adr32.nc")
+    nc = netCDF4.Dataset(filename)
+    nceval = define_class(nc)
+    bp = BoolParser(nceval)
+    test = """(SOLAR_ZENITHAL_ANGLE>55)&(SOLAR_ZENITHAL_ANGLE<56)"""
+    for line in test.split("\n"):
+        res = bp.parse(line)
+        assert (res.size == 101)
+        # if any(res):
+        # print (nc.variables["SOLAR_ZENITHAL_ANGLE"][res])
 
 
-test = """(Profile_lat>=-5.)&(Profile_lat<=5.)&(Profile_lon>0)&(Profile_lon<5)"""
-
-for line in test.split("\n"):
-    print line
-    res = bp.parse(line)
-    print res
-    if any(res):
-        print nc.variables["Profile_lat"][res]
+if __name__=="__main__":
+    test_nc()
